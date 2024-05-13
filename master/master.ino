59419eae2833a4148e6276100d4b343151878aa0
AjayGautam1199 <107488026+AjayGautam1199@users.noreply.github.com>
2024-05-13 15:37:09 +0530

    use arduino ide

---
+++ master/master.ino
@@ -0,0 +1,62 @@
+// const int LED_PIN = 16;
+// const int BUTTON_PIN = 12;
+
+// HardwareSerial Serial1(1);
+
+// void setup() {
+//   pinMode(LED_PIN, OUTPUT);
+//   pinMode(BUTTON_PIN, INPUT_PULLUP);
+//   Serial.begin(9600);
+//   Serial1.begin(9600, SERIAL_8N1, 2, 4); // RX, TX pins for Serial1
+// }
+
+// void loop() {
+//   if (Serial1.available() > 0) {
+//     char incomingByte = Serial1.read();
+//     if (incomingByte == '1') {
+//       digitalWrite(LED_PIN, HIGH);
+//       Serial.println("LED turned ON by Arduino Uno");
+//     } else if (incomingByte == '0') {
+//       digitalWrite(LED_PIN, LOW);
+//       Serial.println("LED turned OFF by Arduino Uno");
+//     }
+//   }
+//   if (digitalRead(BUTTON_PIN) == LOW) {
+//     Serial1.println("1"); // Send signal to Arduino Uno
+//     delay(1000); // Debounce delay
+//   }
+//   delay(100);
+// }
+
+
+// use this 
+#include <Arduino.h>
+
+#define LED_PIN 2
+const int uartRxpin = 16;
+
+void setup() 
+{
+    Serial.begin(115200);
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() 
+{
+    if (Serial.available() > 0) 
+    {
+        String input = Serial.readStringUntil('\n');
+        if (input.startsWith("D:"))
+         {
+            int distance = input.substring(2).toInt();
+            if (distance < 20) 
+            {
+                digitalWrite(LED_PIN, HIGH); // Turn on LED
+            } 
+            else
+             {
+                digitalWrite(LED_PIN, LOW); // Turn off LED
+             }
+        }
+    }
+}
---
+++ slave/slave.ino
@@ -0,0 +1,60 @@
+// const int LED_PIN = 13;
+// const int BUTTON_PIN = A0;
+
+// void setup() {
+//   pinMode(LED_PIN, OUTPUT);
+//   pinMode(BUTTON_PIN, INPUT_PULLUP);
+//   Serial.begin(9600);
+// }
+
+// void loop() {
+//   if (Serial.available() > 0) {
+//     char incomingByte = Serial.read();
+//     if (incomingByte == '1') {
+//       digitalWrite(LED_PIN, HIGH);
+//       Serial.println("LED turned ON by ESP32");
+//     } else if (incomingByte == '0') {
+//       digitalWrite(LED_PIN, LOW);
+//       Serial.println("LED turned OFF by ESP32");
+//     }
+//   }
+//   if (digitalRead(BUTTON_PIN) == LOW) {
+//     Serial.println("1"); // Send signal to ESP32
+//     delay(1000); // Debounce delay
+//   }
+//   delay(100);
+// }
+
+
+#include <Arduino.h>
+
+const int trigPin = 5; // Trig pin of ultrasonic sensor
+const int echoPin = 18; // Echo pin of ultrasonic sensor
+const int uartTxPin = 17; // UART TX pin
+
+void setup() {
+  Serial.begin(9600);
+  Serial2.begin(9600, SERIAL_8N1, uartTxPin); // Initialize UART2
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) / 29.1; // Convert to centimeters
+
+  Serial.print("D:");
+  Serial.println(distance);
+
+  // Transmit distance over UART2
+  Serial2.print("D:");
+  Serial2.println(distance);
+
+  delay(3000); // Adjust delay as needed
+}
